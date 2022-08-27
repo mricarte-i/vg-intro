08182d1ba59bc32dd26b09b8ad2f9a75adbcf0b7
mricarte <mricarte@itba.edu.ar>
2022-08-27 12:06:48 -0300

    based of some guys blog on cinemachine & fg

--- Assets/Scripts/PlayerGuider.cs
+++ Assets/Scripts/PlayerGuider.cs
@@ -1,46 +1,35 @@
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class PlayerGuider : MonoBehaviour
 {
-    [SerializeField] private Transform[] _players;
+    [SerializeField] private Transform _p1, _p2;
     [SerializeField] private float _height = 1.25f;
-
-    private Camera _camera;
-    private float _aspectRatio;
-    private float _tanFOV;
-    [SerializeField] private float _distanceMargin = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (_players.Length != 2)
+        if (_p1 == null || _p2 == null)
         {
             Debug.LogError("Player Guider requires both players to be referenced!");
-            Debug.Break();
         }
-        _camera = GetComponentInChildren<Camera>();
-
-        _camera.transform.rotation = Quaternion.Euler(0, -90, 0);
-        _camera.transform.position = new Vector3(3, 0, 0);
-
-        _aspectRatio = Screen.width / Screen.height;
-        _tanFOV = Mathf.Tan(Mathf.Deg2Rad * _camera.fieldOfView / 2.0f);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        var player1Pos = _players[0].position;
-        var player2Pos = _players[1].position;
-        var middle = (player1Pos + player2Pos) / 2;
-        var dist = Vector3.Distance(player1Pos, player2Pos);
-        _players[0].LookAt(new Vector3(player2Pos.x, player1Pos.y, player2Pos.z));
-        _players[1].LookAt(new Vector3(player1Pos.x, player2Pos.y, player1Pos.z));
+        if (_p1 == null || _p2 == null)
+        {
+            Debug.LogError("Player Guider requires both players to be referenced!");
+            return;
+        }
+        var player1Pos = _p1.position;
+        var player2Pos = _p2.position;
+        _p1.LookAt(new Vector3(player2Pos.x, player1Pos.y, player2Pos.z));
+        _p2.LookAt(new Vector3(player1Pos.x, player2Pos.y, player1Pos.z));
 
+        var middle = (player1Pos + player2Pos) / 2;
         transform.position = middle + Vector3.up*_height;
         transform.LookAt(new Vector3(player2Pos.x, transform.position.y, player2Pos.z));
-
-        var cameraDist = (dist / 2 / _aspectRatio) / _tanFOV;
-        _camera.transform.position = transform.position + transform.right * (cameraDist + _distanceMargin);
     }
 }
