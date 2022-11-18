736dd13c531c7767ef7df8db929d793bef5b0486
mricarte <mricarte@itba.edu.ar>
2022-11-18 01:52:21 -0300

    add 1 raycast attack to dagum, also some vfx & sfx

---
+++ Assets/Scripts/Attacks/RaycastAttack.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using Controllers;
+using UnityEngine;
+
+namespace Attacks
+{
+  public class RaycastAttack : Attack {
+
+        [SerializeField] private GameObject _beamPrefab;
+        [SerializeField] private float _range = 6f;
+        private GameObject _spawnedBeam;
+
+        private bool _endingAttack = false;
+
+        public override void Execute(){
+            _isAttacking = true;
+            InvokeBeforeAttackingEvents();
+            _spawnedBeam = Instantiate(_beamPrefab, transform);
+            var sfxgo = Instantiate(_sfxPrefab);
+            sfxgo.GetComponent<SoundEffectController>().Play(_attackStats.SFXTHROW);
+
+            StartCoroutine(Wait(Duration));
+        }
+
+        private void Update(){
+            if(_isAttacking && !_endingAttack) {
+                RaycastHit hit;
+                if(Physics.Raycast(transform.position, transform.forward, out hit, _range)){
+                    var lifeControllerHit = hit.transform.gameObject.GetComponent<LifeController>();
+
+                    if(lifeControllerHit != null && lifeControllerHit != _hurtbox){
+                        base.MakeDamage(lifeControllerHit);
+                    }
+                }
+            }
+        }
+
+        private IEnumerator Wait(float duration)
+        {
+            yield return new WaitForSecondsRealtime(duration);
+            _endingAttack = true;
+            if(_spawnedBeam != null){
+                Destroy(_spawnedBeam);
+                _spawnedBeam = null;
+            }
+            _isAttacking = false;
+            _endingAttack = false;
+            InvokeAfterAttackingEvents();
+        }
+
+    }
+}
